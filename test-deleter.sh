2b66789b041e62cf2dce672ec422c366842eea9e
abdullahishs <abdullahi@sendible.com>
2022-01-05 15:45:04 +0000

    example workflow

--- test-deleter.sh
+++ test-deleter.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-start_date="2010-01-01"
-end_date=$(date -I --date="100 day ago")
+default_start="2010-01-01"
+default_end="2010-12-12"
+default_end=$(date -I -d "-2400:00:00") #100 days ago
+actual_start=${start_date:=$default_start}
+actual_end=${end_date:=$default_end}
 
-echo Deleting tests from $end_date to $start_date 
+echo Deleting tests from $actual_end till $actual_start 
 
 curl $cypress_url -H 'Accept-Encoding: gzip, deflate, br' \
 -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: $cypress_url' \
---data-binary '{"query":"mutation {\n    deleteRunsInDateRange(startDate: \"'$start_date'T00:00:00.000Z\", endDate: \"'$end_date'T00:00:00.000Z\") {\n      success\n      message\n      runIds\n      __typename\n    }\n  }\n"}' \
+--data-binary '{"query":"mutation {\n    deleteRunsInDateRange(startDate: \"'$actual_start'T00:00:00.000Z\", endDate: \"'$actual_end'T00:00:00.000Z\") {\n      success\n      message\n      runIds\n      __typename\n    }\n  }\n"}' \
 --compressed > /home/output.txt
 
 success=$(jq .data.deleteRunsInDateRange.success /home/output.txt)
